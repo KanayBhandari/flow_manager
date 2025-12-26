7d38ba0bb3acbbddb2827ccae2ee5b698b0af4f7
Kanay Bhandari <kanaybhandari2511@gmail.com>
2025-12-26 20:05:21 +0530

    added github actions cicd pipeline stages for automatic deployment of code to lambda

--- app/main.py
+++ app/main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
 from app.api.flow_routes import router as flow_router
 from app.api.flow_read_routes import router as flow_read_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173", "https://d36wquhowtcyl5.cloudfront.net"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.options("/{path:path}")
+async def options_handler(path: str):
+    return {}
 
 app.include_router(flow_router)
 app.include_router(flow_read_router)
